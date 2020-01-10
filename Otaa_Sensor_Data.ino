d8cdc6a518329413a938a389d34f4d4db37c86e3
Danny <danny.296@hetnet.nl>
2020-01-10 12:28:13 +0100

    Toegevoegd ruimtecode

---
+++ Otaa_Sensor_Data.ino
@@ -0,0 +1,262 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses OTAA (Over-the-air activation), where where a DevEUI and
+ * application key is configured, which are used in an over-the-air
+ * activation procedure where a DevAddr and session keys are
+ * assigned/generated for use with all further communication.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate an AppEUI, DevEUI and AppKey.
+ * Multiple devices can use the same AppEUI, but each device has its own
+ * DevEUI and AppKey.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include <SharpIR.h>
+
+#define IRPin1 A0
+#define IRPin2 A1
+#define model 1080
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8]={ 0x22, 0x58, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8]={ 0x73, 0x7A, 0x81, 0x93, 0x7E, 0x08, 0x55, 0x00 };
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
+static const u1_t PROGMEM APPKEY[16] = { 0x54, 0x32, 0x1C, 0x01, 0x4F, 0x98, 0x2B, 0x49, 0x60, 0x00, 0x92, 0xC4, 0x5B, 0x4B, 0xF4, 0x36 };
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+static osjob_t sendjob;
+static osjob_t scanjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+uint16_t inRuimte = 0;
+uint8_t airQuality = 0;
+uint16_t ruimteCode = 2;
+
+bool active1 = false;
+bool active2 = false;
+String dir = "";
+bool contiIn = false;
+bool contiOut = false;
+
+// Create a new instance of the SharpIR class:
+SharpIR mySensor1 = SharpIR(IRPin1, model);
+SharpIR mySensor2 = SharpIR(IRPin2, model);
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+
+            // Disable link check validation (automatically enabled
+            // during join, but not supported by TTN at this time).
+            LMIC_setLinkCheckMode(0);
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.println(F("Received "));
+              Serial.println(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void do_send(osjob_t* j){
+    airQuality = analogRead(2);
+    byte payload[6];
+    payload[0] = lowByte(inRuimte);
+    payload[1] = highByte(inRuimte);
+    payload[2] = lowByte(airQuality);
+    payload[3] = highByte(airQuality);
+    payload[4] = lowByte(ruimteCode);
+    payload[5] = highByte(ruimteCode);
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, payload, sizeof(payload), 0);
+        Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void do_scan(osjob_t* j){
+  int distance_cm1 = mySensor1.distance();
+  int distance_cm2 = mySensor2.distance();
+
+    if(distance_cm1 > 70){
+      contiIn = true;
+    }
+
+    if(distance_cm2 > 70){
+      contiOut = true;
+    }
+
+    if (distance_cm1 <  70 && contiIn == true) {
+      if (dir == "") {
+        dir = "in";
+        active1 = true;
+      } 
+      else if(dir == "in"){
+        dir = "";
+        active1 = false;
+      }
+      else{
+        active1 = true;
+      }
+      contiIn = false;
+      Serial.println(dir);
+    }
+
+    if (distance_cm2 < 70 && contiOut == true)  {
+      if (dir == "") {
+        dir = "out";
+        active2 = true;
+      }
+      else if(dir == "out"){
+        dir = "";
+        active2 = false;
+      }
+      else{
+        active2 = true;
+      }
+      contiOut = false;
+      Serial.println(dir);
+   }
+
+   if (active1 == true && active2 == true) {
+    if (dir == "in") {
+      inRuimte ++;
+    } else {
+      if(inRuimte == 0){}
+      else{
+        inRuimte --;
+      }
+    }
+    dir = "";
+    active1 = false;
+    active2 = false;
+    Serial.println(inRuimte);
+  }
+
+  os_setTimedCallback(&scanjob, os_getTime()+500, do_scan);
+}
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("Starting"));
+
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Start job (sending automatically starts OTAA too)
+    do_send(&sendjob);
+    do_scan(&scanjob);
+}
+
+void loop() {
+    os_runloop_once();
+}
